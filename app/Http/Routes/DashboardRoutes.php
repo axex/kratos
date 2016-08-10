99d0f96b6fd2bb743469056e1e6ddc242aa5c808
Choi <novnan@126.com>
2016-08-10 14:04:17 +0800

    路由分组

---
+++ app/Http/Routes/DashboardRoutes.php
@@ -0,0 +1,94 @@
+<?php
+namespace App\Http\Routes;
+
+use Illuminate\Routing\Router;
+
+class DashboardRoutes
+{
+    /*
+    |--------------------------------------------------------------------------
+    |  管理员后台 实现文章和用户等管理操作
+    |--------------------------------------------------------------------------
+    */
+    public function map(Router $router)
+    {
+        $router->group(['prefix' => 'dashboard', 'middleware' => 'auth', 'namespace' => 'Dashboard'], function ($router) {
+
+            $as = 'dashboard.';
+
+            # 后台首页
+            $router->get('/', ['as' => $as . 'console', 'uses' => 'AdminController@console']);
+
+            #--------------------
+            # 控制面板
+            #--------------------
+
+            # 个人资料
+            $router->get('me', ['as' => $as . 'me', 'uses' => 'AdminMeController@me']);
+            $router->post('me', 'AdminMeController@meProfile');
+
+            # 头像上传
+            $router->post('avatar', ['as' => $as . 'avatar', 'uses' => 'AdminMeController@avatarUpload']);
+
+
+            #--------------------
+            # 内容管理
+            #--------------------
+
+            # 期数
+            $router->delete('issue/delete', ['as' => $as . 'issue.delete', 'uses' => 'AdminIssueController@batchDelete']);
+            $router->resource('issue', 'AdminIssueController');
+
+            # 文章
+            $router->delete('article/delete', ['as' => $as . 'article.delete', 'uses' => 'AdminArticleController@batchDelete']);
+            $router->resource('article', 'AdminArticleController');
+
+            # 投稿
+            $router->delete('submission/delete', ['as' => $as . 'submission.delete', 'uses' => 'AdminSubmissionController@batchDelete']);
+            $router->resource('submission', 'AdminSubmissionController');
+
+            # 分类
+            $router->resource('category', 'AdminCategoryController');
+
+
+            #--------------------
+            # 用户管理
+            #--------------------
+
+            $router->group(['middleware' => 'deny403:manage_users'], function($router) use ($as) {
+
+                # 后台员
+                $router->resource('user', 'AdminUserController');
+
+                # 订阅用户
+                $router->delete('subscribe/delete', ['as' => $as . 'subscribe.delete', 'uses' => 'AdminSubscribeController@batchDelete']);
+                $router->resource('subscribe', 'AdminSubscribeController');
+
+                # 角色
+                $router->resource('role', 'AdminRoleController');
+
+                # 权限
+                $router->resource('permission', 'AdminPermissionController');
+            });
+
+
+            #--------------------
+            # 系统管理
+            #--------------------
+
+            $router->group(['middleware' => 'deny403:manage_system'], function($router) use ($as) {
+
+                # 系统配置
+                $router->get('system_settings', ['as' => $as . 'system.setting', 'uses' => 'AdminSystemSettingController@index']);
+                $router->post('system_settings', 'AdminSystemSettingController@update');
+
+                # 系统日志
+                $router->get('system_logs', ['as' => $as . 'system.log.index', 'uses' => 'AdminSystemLogController@index']);
+                $router->get('system_logs/{id}', ['as' => $as . 'system.log.show', 'uses' => 'AdminSystemLogController@show']);
+
+                # 导出 excel
+                $router->get('export/excel', ['as' => $as . 'export.excel', 'uses' => 'AdminSystemLogController@excelExport']);
+            });
+        });
+    }
+}
---
+++ app/Http/Routes/FrontendRoutes.php
@@ -0,0 +1,75 @@
+<?php
+namespace App\Http\Routes;
+
+use Illuminate\Routing\Router;
+
+class FrontendRoutes
+{
+    public function map(Router $router)
+    {
+        # 前台首页
+        $router->get('/', ['as' => 'home', 'uses' => 'IndexController@index']);
+
+        /*
+        |--------------------------------------------------------------------------
+        | 订阅邮件
+        |--------------------------------------------------------------------------
+        */
+
+        $router->group(['prefix' => 'subscribe'], function ($router) {
+
+            # 订阅
+            $router->post('', 'SubscribeController@index');
+
+            # 修改订阅资料
+            $router->get('profile/{confirm_code}', 'SubscribeController@profile');
+            $router->post('profile', 'SubscribeController@updateProfile');
+
+            # 邮箱确认订阅
+            $router->get('resend/{confirm_code}', 'SubscribeController@resendEmail');
+            $router->get('{confirm_code}/{email}', 'SubscribeController@confirmEmail');
+        });
+
+        # 取消订阅
+        $router->get('unsubscribe/{confirm_code}', ['as' => 'unsubscribe', 'uses' => 'SubscribeController@unsubscribe']);
+        $router->delete('unsubscribe/{confirm_code}', 'SubscribeController@delete');
+
+        # 期数详情页
+        $router->get('issue{issue}', ['as' => 'issue', 'uses' => 'IssueController@index']);
+        $router->get('search', ['as' => 'search', 'uses' => 'IssueController@search']);
+
+        # 投稿
+        $router->get('add', ['as' => 'add', 'uses' => 'SubmissionController@index']);
+        $router->post('add', 'SubmissionController@store');
+
+
+        /*
+        |--------------------------------------------------------------------------
+        | 登录注册找回密码等操作
+        |--------------------------------------------------------------------------
+        */
+
+        $router->group(['prefix' => 'auth'], function ($router) {
+            $authority = 'AuthorityController@';
+
+            # 登录
+            $router->get('login', ['as' => 'login', 'uses' => $authority . 'getLogin']);
+            $router->post('login', $authority . 'postLogin');
+
+            # 注册
+            $router->get('register', ['as' => 'register', 'uses' => $authority . 'getRegister']);
+            $router->post('register', $authority . 'postRegister');
+
+            # 退出
+            $router->get('logout', ['as' => 'logout', 'uses' => $authority . 'logout']);
+
+            # 密码重置链接请求路由
+            $router->get('reset/password', ['as' => 'reset.password', 'uses' => $authority . 'getEmail']);
+            $router->post('reset/password', $authority . 'postEmail');
+
+            # 密码重置路由
+            $router->get('reset/confirm/{reset_code}', $authority . 'getReset');
+            $router->post('reset/confirm', ['as' => 'reset.confirm', 'uses' => $authority . 'postReset']);
+        });
+    }
+}
